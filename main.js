767c16d11786dc6c5928d25578e1721960b05387
syonosuke <syonosuke1996@outlook.jp>
2023-11-21 01:06:40 +0900

    関数まで

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const userInput = '10';
-let calcResult ;
-calcResult = Number(userInput) + 10; /*Number関数で数値化*/ 
-console.log(calcResult);
+function add(num1,num2){
+  const value = num1 + num2;
+  return value;
+}
+console.log(value);
+const returnedValue = add(2, 3);
