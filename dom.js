c159fa6ab670f9b69d55043c3d5707d3ce84eb80
syonosuke <syonosuke1996@outlook.jp>
2023-11-23 20:52:22 +0900

    fix commit

--- dom.js
+++ dom.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 const todoInput = document.querySelector('todo-input');
 const addTask = document.querySelector('add-button');
 
+=======
+let result = document.documentElement;
+result = document.childNodes;
+console.dir(result);
+>>>>>>> 74ef6693e1a4beabaaf67345c390adf7099b561b
